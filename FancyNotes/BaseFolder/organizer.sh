8bb0b35880eba0b80761c9bb21208ff9630c3709
Gabriel Petrini da Silveira <34845115+gpetrini@users.noreply.github.com>
2020-05-10 09:59:40 -0300

    Add files via upload

---
+++ FancyNotes/BaseFolder/organizer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv *.pdf ./pdfs/
+mv *.aux ./auxs/
+mv *.bcf ./auxs/
+mv *.log ./auxs/
+mv *.out ./auxs/
+mv *.bbl ./auxs/
+mv *.blg ./auxs/
+mv *.run.xml ./auxs/
+mv *.synctex.gz ./auxs/
+
